ef5335267469140399095709f84158b48dc43469
Aleksei Savin <savin.alexey.m@gmail.com>
2025-11-17 21:10:24 +0000

    CLI: prefill step size

--- crates/cli/src/handlers/serve.rs
+++ crates/cli/src/handlers/serve.rs
@@ -2,7 +2,10 @@ use tokio::runtime::Runtime;
 
 use crate::server::main::run_server;
 
-pub fn handle_serve(model_path: String) {
+pub fn handle_serve(
+    model_path: String,
+    prefill_step_size: Option<usize>,
+) {
     let runtime = Runtime::new().unwrap();
-    runtime.block_on(run_server(model_path));
+    runtime.block_on(run_server(model_path, prefill_step_size));
 }
