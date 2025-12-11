18b1f0ca15db369a655d8871131c88f65eeaaefb
eugene <eugenebokhan@icloud.com>
2025-12-11 12:33:15 +0000

    remove unused stuff

--- crates/uzu/src/lib.rs
+++ crates/uzu/src/lib.rs
@@ -14,7 +14,4 @@ pub mod utils;
 pub use utils::*;
 pub mod device;
 pub use device::*;
-pub mod runners;
-// Re-export all configuration types at the crate root so modules can import via `crate::{Type}`
 pub use config::*;
-pub use runners::*;
