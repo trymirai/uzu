b274466d426c564490611667835d604309e87afd
eugene <eugenebokhan@icloud.com>
2026-02-12 16:39:10 +0000

    generalize quantized embedding lookup

--- crates/uzu/src/backends/metal/encodable_block/embedding/mod.rs
+++ crates/uzu/src/backends/metal/encodable_block/embedding/mod.rs
@@ -2,15 +2,17 @@
 
 mod error;
 mod full_precision_readout;
-mod quantized_lookup;
 mod quantized_readout;
 
 pub use error::EmbeddingError;
 pub use full_precision_readout::FullPrecisionEmbeddingReadout;
-pub use quantized_lookup::QuantizedEmbeddingLookup;
 pub use quantized_readout::QuantizedEmbeddingReadout;
 
 use super::super::Metal;
-use crate::encodable_block::FullPrecisionEmbeddingLookup as GenericFullPrecisionEmbeddingLookup;
+use crate::encodable_block::{
+    FullPrecisionEmbeddingLookup as GenericFullPrecisionEmbeddingLookup,
+    QuantizedEmbeddingLookup as GenericQuantizedEmbeddingLookup,
+};
 
 pub type FullPrecisionEmbeddingLookup = GenericFullPrecisionEmbeddingLookup<Metal>;
+pub type QuantizedEmbeddingLookup = GenericQuantizedEmbeddingLookup<Metal>;
