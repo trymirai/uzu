b8e6cb5363d2100b2967b177e21502370010f7f6
eugene <eugenebokhan@icloud.com>
2026-02-24 21:24:31 +0000

    fix test_context_mode (#221)
    * fix test_context_mode
    
    * set warps_per_row 2

--- crates/uzu/src/backends/metal/kernel/matmul/gemm/specialization.rs
+++ crates/uzu/src/backends/metal/kernel/matmul/gemm/specialization.rs
@@ -21,57 +21,47 @@ impl Specialization {
         let prefer_half_or_tf32 = !is_float32 || context.tf32_enabled();
 
         let (block_rows, block_cols, block_depth, warps_per_row, warps_per_col, swizzle_log2) =
-            if context.is_nax_available() && prefer_half_or_tf32 {
-                let tile_rows = (arguments.batch + 128 - 1) / 128;
-                let swizzle_log2 = if tile_rows <= 3 {
-                    0
-                } else {
-                    1
-                };
-                (128, 128, 512, 4, 4, swizzle_log2)
-            } else {
-                match context.device_class() {
-                    DeviceClass::Integrated | DeviceClass::Phone | DeviceClass::Unknown(_) => {
+            match context.device_class() {
+                DeviceClass::Integrated | DeviceClass::Phone | DeviceClass::Unknown(_) => {
+                    if prefer_half_or_tf32 {
+                        if arguments.transpose_b {
+                            (64, 32, 32, 2, 2, 0)
+                        } else {
+                            (64, 64, 16, 2, 2, 0)
+                        }
+                    } else if arguments.transpose_b {
+                        (32, 64, 16, 2, 2, 0)
+                    } else {
+                        (64, 32, 32, 2, 2, 0)
+                    }
+                },
+                DeviceClass::Desktop => {
+                    if overall_work_elements >= (1_i64 << 20) {
                         if prefer_half_or_tf32 {
-                            if arguments.transpose_b {
-                                (64, 32, 32, 2, 2, 0)
-                            } else {
-                                (64, 64, 16, 1, 2, 0)
-                            }
-                        } else if arguments.transpose_b {
-                            (32, 64, 16, 1, 2, 0)
-                        } else {
-                            (64, 32, 32, 2, 2, 0)
-                        }
-                    },
-                    DeviceClass::Desktop => {
-                        if overall_work_elements >= (1_i64 << 20) {
-                            if prefer_half_or_tf32 {
-                                if 2 * std::cmp::max(arguments.batch, arguments.output_dim) > arguments.input_dim {
-                                    (64, 64, 16, 2, 2, 0)
-                                } else if arguments.transpose_b {
-                                    (64, 32, 32, 2, 2, 0)
-                                } else {
-                                    (32, 64, 16, 1, 2, 0)
-                                }
+                            if 2 * std::cmp::max(arguments.batch, arguments.output_dim) > arguments.input_dim {
+                                (64, 64, 16, 2, 2, 0)
                             } else if arguments.transpose_b {
-                                (32, 64, 16, 1, 2, 0)
-                            } else {
-                                (64, 32, 32, 2, 2, 0)
-                            }
-                        } else if prefer_half_or_tf32 {
-                            if arguments.transpose_b {
                                 (64, 32, 32, 2, 2, 0)
                             } else {
-                                (64, 64, 16, 1, 2, 0)
+                                (32, 64, 16, 2, 2, 0)
                             }
                         } else if arguments.transpose_b {
-                            (32, 64, 16, 1, 2, 0)
+                            (32, 64, 16, 2, 2, 0)
                         } else {
                             (64, 32, 32, 2, 2, 0)
                         }
-                    },
-                }
+                    } else if prefer_half_or_tf32 {
+                        if arguments.transpose_b {
+                            (64, 32, 32, 2, 2, 0)
+                        } else {
+                            (64, 64, 16, 2, 2, 0)
+                        }
+                    } else if arguments.transpose_b {
+                        (32, 64, 16, 2, 2, 0)
+                    } else {
+                        (64, 32, 32, 2, 2, 0)
+                    }
+                },
             };
 
         let m = arguments.batch;
