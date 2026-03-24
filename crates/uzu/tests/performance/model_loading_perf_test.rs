4c99b59a4c1f69093bf7c663fb2404071b26f743
Aleksandr Golokoz <5617556+agolokoz@users.noreply.github.com>
2026-03-24 22:31:06 +0300

    Fix iterations count

--- crates/uzu/tests/performance/model_loading_perf_test.rs
+++ crates/uzu/tests/performance/model_loading_perf_test.rs
@@ -5,7 +5,7 @@ use crate::common::{path::get_test_model_path, perf::run_perf};
 #[test]
 #[ignore]
 fn test_perf_model_loading() {
-    let perf_results = run_perf("Model loading", 5, || {
+    let perf_results = run_perf("Model loading", 10, || {
         let model_path = get_test_model_path();
         let _ = ChatSession::new(model_path, DecodingConfig::default());
     });
