6f6ddaa9787bcefaa36bae4a625b34f0645ccd1a
Aleksandr Golokoz <5617556+agolokoz@users.noreply.github.com>
2026-04-07 22:55:10 +0300

    Quant matmul cpu kernels (#316)
    * Generalize quant_matmul_test.rs
    
    * Add quantized_matmul_qmm() implementation
    
    * Add qmm_test.rs
    
    * Add qmm_transposed_test.rs
    
    * Add qmm_transposed_wide_test.rs
    
    * Add qmm_transposed implementation
    
    * Add qmv.rs implementation
    
    * Add qmv_test.rs
    
    * Add qmv_fast_test.rs
    
    * Fix comments
    
    * Fix qmm_transposed tolerance
    
    * Fix qmm_transposed
    
    * Ignore tests
    
    * Fixes

--- crates/uzu/src/backends/cpu/kernel/quant_matmul/qmv_fast.rs
+++ crates/uzu/src/backends/cpu/kernel/quant_matmul/qmv_fast.rs
@@ -2,32 +2,38 @@ use dsl::kernel;
 use half::{bf16, f16};
 use num_traits::Float;
 
-use crate::ArrayElement;
+use crate::{ArrayElement, backends::cpu::kernel::quant_matmul::qmv::qmv};
 
 #[kernel(QuantizedMatmulQmvFast)]
 #[variants(T, f32, f16, bf16)]
 #[variants(GROUP_SIZE, 32, 64, 128)]
 #[variants(BITS, 4, 8)]
 pub fn quantized_matmul_qmv_fast<T: ArrayElement + Float, const GROUP_SIZE: u32, const BITS: u32>(
-    #[allow(unused)] weights: *const u32,
-    #[allow(unused)] scales: *const T,
-    #[allow(unused)]
-    #[optional(use_zero_points)]
-    zero_points: Option<*const u8>,
-    #[allow(unused)]
-    #[optional(use_mlx_quant)]
-    biases: Option<*const T>,
-    #[allow(unused)] input: *const T,
-    #[allow(unused)] output: *mut T,
-    #[allow(unused)] in_vec_size: u32,
-    #[allow(unused)] out_vec_size: u32,
-    #[allow(unused)] batch_size: u32,
-    #[allow(unused)]
-    #[specialize]
-    use_zero_points: bool,
-    #[allow(unused)]
-    #[specialize]
-    use_mlx_quant: bool,
+    weights: *const u32,
+    scales: *const T,
+    #[optional(use_zero_points)] zero_points: Option<*const u8>,
+    #[optional(use_mlx_quant)] biases: Option<*const T>,
+    input: *const T,
+    output: *mut T,
+    in_vec_size: u32,
+    out_vec_size: u32,
+    batch_size: u32,
+    #[specialize] use_zero_points: bool,
+    #[specialize] use_mlx_quant: bool,
 ) {
-    todo!()
+    qmv::<T>(
+        weights,
+        scales,
+        zero_points,
+        biases,
+        input,
+        output,
+        in_vec_size as usize,
+        out_vec_size as usize,
+        batch_size as usize,
+        use_zero_points,
+        use_mlx_quant,
+        GROUP_SIZE as usize,
+        BITS as usize,
+    );
 }
--- crates/uzu/tests/common/mod.rs
+++ crates/uzu/tests/common/mod.rs
@@ -45,3 +45,21 @@ macro_rules! for_each_non_cpu_backend {
     }};
 }
 pub(crate) use for_each_non_cpu_backend;
+
+macro_rules! for_each_float_type {
+    (|$F:ident| $body:expr) => {{
+        {
+            type $F = f32;
+            $body
+        }
+        {
+            type $F = half::f16;
+            $body
+        }
+        {
+            type $F = half::bf16;
+            $body
+        }
+    }};
+}
+pub(crate) use for_each_float_type;
