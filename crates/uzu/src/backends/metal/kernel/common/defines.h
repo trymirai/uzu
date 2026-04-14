6f82528a758d1845e40f5a1f71df8b313639c94f
eugene <eugenebokhan@icloud.com>
2026-04-14 20:37:20 +0100

    add pragma no unroll

--- crates/uzu/src/backends/metal/kernel/common/defines.h
+++ crates/uzu/src/backends/metal/kernel/common/defines.h
@@ -2,6 +2,7 @@
 
 #define METAL_CONST static constant constexpr
 #define METAL_PRAGMA_UNROLL _Pragma("clang loop unroll(full)")
+#define METAL_PRAGMA_NO_UNROLL _Pragma("clang loop unroll(disable)")
 #define METAL_SIMD_SIZE 32
 
 METAL_FUNC int pow2(int n) { return 1 << n; }
