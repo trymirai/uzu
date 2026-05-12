0630b7a77a5eae5937ca8c3a24f6c8f0f1ba8c76
Aleksandr Golokoz <5617556+agolokoz@users.noreply.github.com>
2026-05-12 16:34:33 +0300

    Remove redundant lifetimes

--- crates/backend-uzu/src/backends/common/buffer_range.rs
+++ crates/backend-uzu/src/backends/common/buffer_range.rs
@@ -45,7 +45,7 @@ impl<'a, B: Buffer> BufferRangeRef<'a, B> {
 pub trait AsBufferRangeRef {
     type Buffer: Buffer;
 
-    fn as_buffer_range_ref<'a>(&'a self) -> BufferRangeRef<'a, Self::Buffer>;
+    fn as_buffer_range_ref(&self) -> BufferRangeRef<'_, Self::Buffer>;
 }
 
 pub struct BufferRangeMut<'a, B: Buffer> {
@@ -94,22 +94,20 @@ impl<'a, B: Buffer> BufferRangeMut<'a, B> {
 }
 
 pub trait AsBufferRangeMut: AsBufferRangeRef {
-    fn as_buffer_range_mut<'a>(&'a mut self) -> BufferRangeMut<'a, Self::Buffer>;
+    fn as_buffer_range_mut(&mut self) -> BufferRangeMut<'_, Self::Buffer>;
 }
 
 impl<B: Buffer> AsBufferRangeRef for B {
     type Buffer = B;
 
-    fn as_buffer_range_ref<'a>(&'a self) -> BufferRangeRef<'a, B> {
+    fn as_buffer_range_ref(&self) -> BufferRangeRef<'_, B> {
         BufferRangeRef::new(self, 0..self.size())
     }
 }
 
 impl<B: Buffer> AsBufferRangeMut for B {
-    fn as_buffer_range_mut<'a>(&'a mut self) -> BufferRangeMut<'a, B> {
-        let size = self.size();
-
-        BufferRangeMut::new_exclusive(self, 0..size)
+    fn as_buffer_range_mut(&mut self) -> BufferRangeMut<'_, B> {
+        BufferRangeMut::new_exclusive(self, 0..self.size())
     }
 }
 
