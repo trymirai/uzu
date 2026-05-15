ad07bc91f16cb150809ae6133c152dc3fa9ce138
uuuvn <uuuvn@proton.me>
2026-05-16 01:34:55 +0400

    Rename dsl crate to proc-macros (because it's not just dsl now)

---
+++ crates/proc-macros/src/lib.rs
@@ -0,0 +1,36 @@
+mod dsl;
+mod uzu_test;
+
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn kernel(
+    args: TokenStream,
+    input: TokenStream,
+) -> TokenStream {
+    dsl::kernel(args, input)
+}
+
+#[proc_macro_attribute]
+pub fn __internal_uzu_test(
+    args: TokenStream,
+    input: TokenStream,
+) -> TokenStream {
+    uzu_test::__internal_uzu_test(args, input)
+}
+
+#[proc_macro_attribute]
+pub fn __internal_uzu_bench(
+    args: TokenStream,
+    input: TokenStream,
+) -> TokenStream {
+    uzu_test::__internal_uzu_bench(args, input)
+}
+
+#[proc_macro_attribute]
+pub fn __internal_uzu_ignored(
+    args: TokenStream,
+    input: TokenStream,
+) -> TokenStream {
+    uzu_test::__internal_uzu_ignored(args, input)
+}
