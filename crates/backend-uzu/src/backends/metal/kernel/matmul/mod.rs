e58f4a2bd683c25241668a2326aa33140f94c864
eugene <eugenebokhan@icloud.com>
2026-05-25 14:38:22 +0100

    restore gemv post-op TODO comment

--- crates/backend-uzu/src/backends/metal/kernel/matmul/mod.rs
+++ crates/backend-uzu/src/backends/metal/kernel/matmul/mod.rs
@@ -165,6 +165,7 @@ impl MatmulMetalKernel {
             k,
         } = arguments;
 
+        // A temopary hack, will port the post ops to the gemv later.
         let d_transform = d_transform.without(GemmDTransform::RHT);
 
         gemv::fp::encode(
