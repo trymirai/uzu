0d352d8fd01ff0f37ca6fb2089aca99c7a4e0773
Aleksandr Golokoz <5617556+agolokoz@users.noreply.github.com>
2026-06-12 19:14:32 +0300

    Finished migration benchmarks to custom harness

--- crates/backend-uzu/tests/integration_entry.rs
+++
@@ -1,4 +0,0 @@
-// #![feature(custom_test_frameworks, test)]
-// #![test_runner(crate::harness::uzu_harness)]
-//
-// mod integration;
--- crates/backend-uzu/tests/performance_entry.rs
+++
@@ -1,5 +0,0 @@
-// #![feature(custom_test_frameworks, test)]
-// #![test_runner(crate::harness::uzu_harness)]
-//
-// mod common;
-// mod performance;
