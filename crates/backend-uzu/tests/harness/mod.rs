b98378380e85e448c0a541998a216b64e41d0d79
Aleksandr Golokoz <5617556+agolokoz@users.noreply.github.com>
2026-06-12 17:50:15 +0300

    Add running benches

--- crates/backend-uzu/tests/harness/mod.rs
+++ crates/backend-uzu/tests/harness/mod.rs
@@ -4,15 +4,30 @@ mod uzu_test;
 
 pub extern crate test;
 
+use test::TestDescAndFn;
 pub use uzu_test::UzuTest;
 
 pub fn uzu_harness(tests: &[&UzuTest]) {
-    let tests = tests
-        .iter()
-        .filter_map(|test| match test {
-            UzuTest::Test(test) => Some(*test),
-            UzuTest::Bench(_) => None,
-        })
-        .collect::<Vec<_>>();
-    test::test_main_static(&tests)
+    let args = std::env::args().collect::<Vec<String>>();
+    let benchmarks = args.contains(&"--benches".to_string());
+    println!("args: {:?}", args);
+    if benchmarks {
+        let bench_tests: Vec<&dyn Fn()> = tests
+            .iter()
+            .filter_map(|test| match test {
+                UzuTest::Bench(test) => Some(*test),
+                UzuTest::Test(_) => None,
+            })
+            .collect::<Vec<_>>();
+        criterion::runner(bench_tests.as_slice());
+    } else {
+        let default_tests: Vec<&TestDescAndFn> = tests
+            .iter()
+            .filter_map(|test| match test {
+                UzuTest::Bench(_) => None,
+                UzuTest::Test(test) => Some(*test),
+            })
+            .collect::<Vec<_>>();
+        test::test_main_static(&default_tests)
+    }
 }
