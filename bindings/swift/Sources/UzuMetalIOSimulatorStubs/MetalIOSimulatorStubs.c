0c0ce157efa84223149a8a1807f4070e16248c23
eugene <eugenebokhan@icloud.com>
2026-06-25 15:32:37 +0100

    bindings: clang-format metal simulator stubs

--- bindings/swift/Sources/UzuMetalIOSimulatorStubs/MetalIOSimulatorStubs.c
+++ bindings/swift/Sources/UzuMetalIOSimulatorStubs/MetalIOSimulatorStubs.c
@@ -5,40 +5,30 @@ void uzu_metal_io_simulator_stubs_anchor(void) {}
 
 #if TARGET_OS_SIMULATOR
 
-typedef void *MTLIOCompressionContext;
+typedef void* MTLIOCompressionContext;
 typedef long MTLIOCompressionMethod;
 typedef long MTLIOCompressionStatus;
 
-const void *MTLIOErrorDomain = 0;
+const void* MTLIOErrorDomain = 0;
 
-size_t MTLIOCompressionContextDefaultChunkSize(void) {
-    return 0;
+size_t MTLIOCompressionContextDefaultChunkSize(void) { return 0; }
+
+MTLIOCompressionContext MTLIOCreateCompressionContext(const char* path, MTLIOCompressionMethod type, size_t chunkSize) {
+  (void)path;
+  (void)type;
+  (void)chunkSize;
+  return NULL;
 }
 
-MTLIOCompressionContext MTLIOCreateCompressionContext(
-    const char *path,
-    MTLIOCompressionMethod type,
-    size_t chunkSize
-) {
-    (void)path;
-    (void)type;
-    (void)chunkSize;
-    return NULL;
-}
-
-void MTLIOCompressionContextAppendData(
-    MTLIOCompressionContext context,
-    const void *data,
-    size_t size
-) {
-    (void)context;
-    (void)data;
-    (void)size;
+void MTLIOCompressionContextAppendData(MTLIOCompressionContext context, const void* data, size_t size) {
+  (void)context;
+  (void)data;
+  (void)size;
 }
 
 MTLIOCompressionStatus MTLIOFlushAndDestroyCompressionContext(MTLIOCompressionContext context) {
-    (void)context;
-    return 1;
+  (void)context;
+  return 1;
 }
 
 #endif
