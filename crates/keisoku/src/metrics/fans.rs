2b8a6214f90e6e3b68624d99f130be1e662a8788
eugene <eugenebokhan@icloud.com>
2026-07-03 18:13:13 +0100

    split Fan into its own metrics file

--- crates/keisoku/src/metrics/fans.rs
+++ crates/keisoku/src/metrics/fans.rs
@@ -1,14 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use crate::units::Rpm;
-
-#[derive(Debug, Default, Clone, Serialize, Deserialize)]
-pub struct Fan {
-    pub actual: Rpm,
-    pub minimum: Rpm,
-    pub maximum: Rpm,
-    pub target: Rpm,
-}
+use super::fan::Fan;
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct FanMetrics {
