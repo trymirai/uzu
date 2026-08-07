83d01376a24ed90ad099d3a6f262919be4fd20aa
Aleksandr Golokoz <5617556+agolokoz@users.noreply.github.com>
2026-08-07 13:34:45 +0300

    Fix checking if model exists

--- crates/cli/src/interactive/components/application.rs
+++ crates/cli/src/interactive/components/application.rs
@@ -89,7 +89,8 @@ pub fn Application(
             };
             match engine.model(identifier.clone()).await {
                 Ok(Some(model)) => {
-                    let model_exists = !model.is_local()
+                    let model_exists = model.is_downloadable()
+                        || !model.is_local()
                         || matches!(
                             engine.model_path(&model).await,
                             Some(path) if std::path::Path::new(&path).exists()
