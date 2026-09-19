ee86871fb3f71b5e701c26f9872b71c62c5e62d6
uuuvn <83587632+uuuvn@users.noreply.github.com>
2026-09-20 03:26:22 +0400

    Add AVFAudio for iOS (#844)

--- Package.swift
+++ Package.swift
@@ -31,6 +31,7 @@ let package = Package(
                 .linkedFramework("Metal"),
                 .linkedFramework("CoreAudio"),
                 .linkedFramework("AudioToolbox"),
+                .linkedFramework("AVFAudio", .when(platforms: [.iOS])),
             ]
         ),
         .target(
--- crates/legacy/uzu/bindings/swift/Package.swift
+++ crates/legacy/uzu/bindings/swift/Package.swift
@@ -29,6 +29,7 @@ let package = Package(
                 .linkedFramework("Metal"),
                 .linkedFramework("CoreAudio"),
                 .linkedFramework("AudioToolbox"),
+                .linkedFramework("AVFAudio", .when(platforms: [.iOS])),
             ]
         ),
         .target(
